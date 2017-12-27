54afd14d562092f594f69363b1a2e49014c3d15f
blueback <blueback28@gmail.com>
2017-12-27 12:47:49 +0900

    整理

--- source/postconnect/app/app.cpp
+++ source/postconnect/app/app.cpp
@@ -110,9 +110,9 @@ namespace NApp
 					}
 				}
 
+				//ＪＳＯＮ解析。
 				if(t_fileobject->GetLoadSize() > 0){
-					sharedptr<JsonItem> t_jsonitem(new JsonItem(reinterpret_cast<char*>(t_fileobject->GetLoadData().get())));
-					return t_jsonitem;
+					return sharedptr<JsonItem>(new JsonItem(reinterpret_cast<char*>(t_fileobject->GetLoadData().get())));
 				}
 
 			}else{
@@ -129,25 +129,19 @@ namespace NApp
 		{
 			this->app_argument = GetEntryParamReference().argument;
 
+			this->filename_config = L"";
+
 			//ファイル名解析。
 			if(this->app_argument){
 				if(this->app_argument->IsExistItem("-input") == true){
 					if(this->app_argument->GetItem("-input")->GetValueType() == JsonItem::ValueType::StringData){
 						this->filename_config = CharToWchar(*this->app_argument->GetItem("-input")->GetStringData().get());
-						if(this->filename_config.length() > 0){
-							std::cout << "input : " << WcharToChar(this->filename_config) << std::endl;
-						}else{
-							std::cout << "error : input" << std::endl;
-							return false;
-						}
-					}else{
-						std::cout << "error : input" << std::endl;
-						return false;
 					}
-				}else{
-					std::cout << "error : input" << std::endl;
-					return false;
 				}
+			}
+
+			if(this->filename_config.length() > 0){
+				std::cout << "input : " << WcharToChar(this->filename_config) << std::endl;
 			}else{
 				std::cout << "error : input" << std::endl;
 				return false;
@@ -162,6 +156,234 @@ namespace NApp
 		}
 
 
+		/** ホスト設定。
+		*/
+		bool SetHostFromConfig()
+		{
+			if(this->config){
+				if(this->config->IsExistItem("host") == true){
+					if(this->config->GetItem("host")->GetValueType() == JsonItem::ValueType::StringData){
+						STLString t_host = *this->config->GetItem("host")->GetStringData();
+						if(t_host.length() > 0){
+							std::cout << "host : " << t_host << std::endl;
+							this->http->SetHost(t_host);
+							return true;
+						}
+					}
+				}
+			}
+
+			std::cout << "error : host" << std::endl;
+			return false;
+		}
+
+
+		/** ポート設定。
+		*/
+		bool SetPortFromConfig()
+		{
+			if(this->config){
+				if(this->config->IsExistItem("port") == true){
+					if(this->config->GetItem("port")->GetValueType() == JsonItem::ValueType::IntegerNumber){
+						s32 t_port = this->config->GetItem("port")->GetInteger();
+						if(t_port > 0){
+							std::cout << "port : " << t_port << std::endl;
+							this->http->SetPort(t_port);
+							return true;
+						}
+					}
+				}
+			}
+
+			std::cout << "error : port" << std::endl;
+			return false;
+		}
+
+
+		/** ＵＲＬ設定。
+		*/
+		bool SetUrlFromConfig()
+		{
+			if(this->config){
+				if(this->config->IsExistItem("url") == true){
+					if(this->config->GetItem("url")->GetValueType() == JsonItem::ValueType::StringData){
+						STLString t_url = *this->config->GetItem("url")->GetStringData();
+						if(t_url.length() > 0){
+							std::cout << "url : " << t_url << std::endl;
+							this->http->SetUrl(t_url);
+							return true;
+						}
+					}
+				}
+			}
+
+			std::cout << "error : url" << std::endl;
+			return false;
+		}
+
+
+		/** アプトプット取得。
+		*/
+		STLWString GetOutPutFromConfig()
+		{
+			if(this->config){
+				if(this->config->GetValueType() == JsonItem::ValueType::AssociativeArray){
+					if(this->config->IsExistItem("output") == true){
+						if(this->config->GetItem("output")->GetValueType() == JsonItem::ValueType::StringData){
+							STLWString t_output_filename;
+							CharToWchar(*this->config->GetItem("output")->GetStringData(),t_output_filename);
+							if(t_output_filename.length() > 0){
+								std::cout << "output : " << WcharToChar(t_output_filename) << std::endl;
+								return t_output_filename;
+							}
+						}
+					}
+				}
+			}
+
+			std::cout << "error : output" << std::endl;
+			return L"";
+		}
+
+
+		/** コンテンツ数取得。
+		*/
+		s32 GetContentsMax()
+		{
+			int t_list_max = -1;
+			{
+				if(this->config->IsExistItem("contents") == true){
+					if(this->config->GetItem("contents")->GetValueType() == JsonItem::ValueType::IndexArray){
+						t_list_max = this->config->GetItem("contents")->GetListMax();
+						if(t_list_max >= 0){
+							std::cout << "contents : " << t_list_max << std::endl;
+							return t_list_max;
+						}
+					}
+				}
+			}
+
+			std::cout << "error : contents" << std::endl;
+			return -1;
+		}
+
+
+		/** タイプ文字取得。
+		*/
+		STLString GetTypeStringFromContentsItem(const sharedptr<JsonItem>& a_jsonitem)
+		{
+			if(a_jsonitem){
+				if(a_jsonitem->GetValueType() == JsonItem::ValueType::AssociativeArray){
+					if(a_jsonitem->IsExistItem("type") == true){
+						if(a_jsonitem->GetItem("type")->GetValueType() == JsonItem::ValueType::StringData){
+							STLString t_type_string = *a_jsonitem->GetItem("type")->GetStringData();
+							if(t_type_string.length() > 0){
+								std::cout << "type : " << t_type_string << std::endl;
+								return t_type_string;
+							}
+						}
+					}
+				}
+			}
+
+			std::cout << "error : type" << std::endl;
+			return "";
+		}
+
+
+		/** パス取得。
+		*/
+		STLWString GetPathFromContentsItem(const sharedptr<JsonItem>& a_jsonitem)
+		{
+			if(a_jsonitem){
+				if(a_jsonitem->GetValueType() == JsonItem::ValueType::AssociativeArray){
+					if(a_jsonitem->IsExistItem("path") == true){
+						if(a_jsonitem->GetItem("path")->GetValueType() == JsonItem::ValueType::StringData){
+							STLWString t_path;
+							CharToWchar(*a_jsonitem->GetItem("path")->GetStringData(),t_path);
+							if(t_path.length() > 0){
+								std::cout << "path : " << WcharToChar(t_path) << std::endl;
+								return t_path;
+							}
+						}
+					}
+				}
+			}
+
+			std::cout << "error : path" << std::endl;
+			return L"";
+		}
+
+
+		/** 名前取得。
+		*/
+		STLString GetNameFromContentsItem(const sharedptr<JsonItem>& a_jsonitem)
+		{
+			if(a_jsonitem){
+				if(a_jsonitem->GetValueType() == JsonItem::ValueType::AssociativeArray){
+					if(a_jsonitem->IsExistItem("name") == true){
+						if(a_jsonitem->GetItem("name")->GetValueType() == JsonItem::ValueType::StringData){
+							STLString t_name = *a_jsonitem->GetItem("name")->GetStringData();
+							if(t_name.length() > 0){
+								std::cout << "name : " << t_name << std::endl;
+								return t_name;
+							}
+						}
+					}
+				}
+			}
+
+			std::cout << "error : name" << std::endl;
+			return "";
+		}
+
+
+		/** 値取得。
+		*/
+		STLString GetValueFromContentsItem(const sharedptr<JsonItem>& a_jsonitem)
+		{
+			if(a_jsonitem){
+				if(a_jsonitem->GetValueType() == JsonItem::ValueType::AssociativeArray){
+					if(a_jsonitem->IsExistItem("value") == true){
+						if(a_jsonitem->GetItem("value")->GetValueType() == JsonItem::ValueType::StringData){
+							STLString t_value = *a_jsonitem->GetItem("value")->GetStringData();
+							if(t_value.length() > 0){
+								std::cout << "value : " << t_value << std::endl;
+								return t_value;
+							}
+						}
+					}
+				}
+			}
+
+			std::cout << "error : value" << std::endl;
+			return "";
+		}
+
+
+		/** ファイル名取得。
+		*/
+		STLString GetFileNameFromContentsItem(const sharedptr<JsonItem>& a_jsonitem)
+		{
+			if(a_jsonitem){
+				if(a_jsonitem->GetValueType() == JsonItem::ValueType::AssociativeArray){
+					if(a_jsonitem->IsExistItem("filename") == true){
+						if(a_jsonitem->GetItem("filename")->GetValueType() == JsonItem::ValueType::StringData){
+							STLString t_filename = *a_jsonitem->GetItem("filename")->GetStringData();
+							if(t_filename.length() > 0){
+								std::cout << "filename : " << t_filename << std::endl;
+								return t_filename;
+							}
+						}
+					}
+				}
+			}
+
+			std::cout << "error : filename" << std::endl;
+			return nullptr;
+		}
+
+
 		/** ＨＴＴＰ作成。
 		*/
 		bool CreateHttp()
@@ -169,216 +391,75 @@ namespace NApp
 			this->http.reset(new NBsys::NHttp::Http());
 			
 			if(this->config){
+
+				//モード設定。
 				this->http->SetMode(NBsys::NHttp::Http_Mode::Post);
 
-				if(this->config->IsExistItem("host") == true){
-					if(this->config->GetItem("host")->GetValueType() == JsonItem::ValueType::StringData){
-						STLString t_host = *this->config->GetItem("host")->GetStringData();
-						if(t_host.length() > 0){
-							std::cout << "host : " << t_host << std::endl;
-							this->http->SetHost(t_host);
-						}else{
-							std::cout << "error : host" << std::endl;
-							return nullptr;
-						}
-					}else{
-						std::cout << "error : host" << std::endl;
-						return nullptr;
-					}
-				}else{
-					std::cout << "error : host" << std::endl;
-					return nullptr;
-				}
+				//ホスト設定。
+				this->SetHostFromConfig();
 
-				if(this->config->IsExistItem("port") == true){
-					if(this->config->GetItem("port")->GetValueType() == JsonItem::ValueType::IntegerNumber){
-						s32 t_port = this->config->GetItem("port")->GetInteger();
-						if(t_port > 0){
-							std::cout << "port : " << t_port << std::endl;
-							this->http->SetPort(t_port);
-						}else{
-							std::cout << "error : port" << std::endl;
-							return nullptr;
-						}
-					}else{
-						std::cout << "error : port" << std::endl;
-						return nullptr;
-					}
-				}else{
-					std::cout << "error : port" << std::endl;
-					return nullptr;
-				}
+				//ポート設定。
+				this->SetPortFromConfig();
 
-				if(this->config->IsExistItem("url") == true){
-					if(this->config->GetItem("url")->GetValueType() == JsonItem::ValueType::StringData){
-						STLString t_url = *this->config->GetItem("url")->GetStringData();
-						if(t_url.length() > 0){
-							std::cout << "url : " << t_url << std::endl;
-							this->http->SetUrl(t_url);
-						}else{
-							std::cout << "error : url" << std::endl;
-							return nullptr;
-						}
-					}else{
-						std::cout << "error : url" << std::endl;
-						return nullptr;
-					}
-				}else{
-					std::cout << "error : url" << std::endl;
-					return nullptr;
-				}
+				//ＵＲＬ設定。
+				this->SetUrlFromConfig();
 			}
 			
 			//コンテンツ作成。
 			{
-				int t_list_max = -1;
-				{
-					if(this->config->IsExistItem("contents") == true){
-						if(this->config->GetItem("contents")->GetValueType() == JsonItem::ValueType::IndexArray){
-							t_list_max = this->config->GetItem("contents")->GetListMax();
-							std::cout << "contents : " << t_list_max << std::endl;
-						}else{
-							std::cout << "error : contents" << std::endl;
-							return nullptr;
-						}
-					}else{
-						std::cout << "error : contents" << std::endl;
-						return nullptr;
-					}
-				}
+				s32 t_list_max = this->GetContentsMax();
 
 				for(int ii=0;ii<t_list_max;ii++){
 					sharedptr<JsonItem> t_content_item = this->config->GetItem("contents")->GetItem(ii);
 
-					STLString t_type_string;
-
-					if(t_content_item->IsExistItem("type") == true){
-						if(t_content_item->GetItem("type")->GetValueType() == JsonItem::ValueType::StringData){
-							t_type_string = *t_content_item->GetItem("type")->GetStringData();
-							if(t_type_string.length() > 0){
-								std::cout << "type : " << t_type_string << std::endl;
-							}else{
-								std::cout << "error : type" << std::endl;
-								return nullptr;
-							}
-						}else{
-							std::cout << "error : type" << std::endl;
-							return nullptr;
-						}
-					}else{
-						std::cout << "error : type" << std::endl;
-						return nullptr;
-					}
+					//タイプ文字取得。
+					STLString t_type_string = this->GetTypeStringFromContentsItem(t_content_item);
 
 					if(t_type_string == "binary"){
 
-						STLWString t_path;
-						STLString t_formname;
-						STLString t_filename;
-
-						if(t_content_item->IsExistItem("path") == true){
-							if(t_content_item->GetItem("path")->GetValueType() == JsonItem::ValueType::StringData){
-								CharToWchar(*t_content_item->GetItem("path")->GetStringData(),t_path);
-								if(t_path.length() > 0){
-									std::cout << "path : " << WcharToChar(t_path) << std::endl;
-								}else{
-									std::cout << "error : path" << std::endl;
-									return nullptr;
-								}
-							}else{
-								std::cout << "error : path" << std::endl;
-								return nullptr;
-							}
-						}else{
-							std::cout << "error : path" << std::endl;
-							return nullptr;
+						//パス取得。
+						STLWString t_path = this->GetPathFromContentsItem(t_content_item);
+						if(t_path.length() <= 0){
+							return false;
 						}
 
-						if(t_content_item->IsExistItem("formname") == true){
-							if(t_content_item->GetItem("formname")->GetValueType() == JsonItem::ValueType::StringData){
-								t_formname = *t_content_item->GetItem("formname")->GetStringData();
-								if(t_formname.length() > 0){
-									std::cout << "formname : " << t_formname << std::endl;
-								}else{
-									std::cout << "error : formname" << std::endl;
-									return nullptr;
-								}
-							}else{
-								std::cout << "error : formname" << std::endl;
-								return nullptr;
-							}
-						}else{
-							std::cout << "error : formname" << std::endl;
-							return nullptr;
+						//名取得。
+						STLString t_name = this->GetNameFromContentsItem(t_content_item);
+						if(t_name.length() <= 0){
+							return false;
 						}
 
-						if(t_content_item->IsExistItem("filename") == true){
-							if(t_content_item->GetItem("filename")->GetValueType() == JsonItem::ValueType::StringData){
-								t_filename = *t_content_item->GetItem("filename")->GetStringData();
-								if(t_filename.length() > 0){
-									std::cout << "filename : " << t_filename << std::endl;
-								}else{
-									std::cout << "error : filename" << std::endl;
-									return nullptr;
-								}
-							}else{
-								std::cout << "error : filename" << std::endl;
-								return nullptr;
-							}
-						}else{
-							std::cout << "error : filename" << std::endl;
-							return nullptr;
+						//ファイル名取得。
+						STLString t_filename = this->GetFileNameFromContentsItem(t_content_item);
+						if(t_filename.length() <= 0){
+							return false;
 						}
 
 						//バイナリの読み込み。
 						sharedptr<NBsys::NFile::File_Object> t_fileobject = this->LoaBinaryFile(t_path);
+						if(t_fileobject == nullptr){
+							return false;
+						}
 
 						//コンテンツの追加。
-						this->http->AddPostContent(t_formname,t_filename,t_fileobject->GetLoadData(),static_cast<s32>(t_fileobject->GetLoadSize()));
+						this->http->AddPostContent(t_name,t_filename,t_fileobject->GetLoadData(),static_cast<s32>(t_fileobject->GetLoadSize()));
 
 					}else if(t_type_string == "param"){
 						
-						STLString t_formname;
-						STLString t_formvalue;
-
-						if(t_content_item->IsExistItem("formname") == true){
-							if(t_content_item->GetItem("formname")->GetValueType() == JsonItem::ValueType::StringData){
-								t_formname = *t_content_item->GetItem("formname")->GetStringData();
-								if(t_formname.length() > 0){
-									std::cout << "forname : " << t_formname << std::endl;
-								}else{
-									std::cout << "error : forname" << std::endl;
-									return nullptr;
-								}
-							}else{
-								std::cout << "error : forname" << std::endl;
-								return nullptr;
-							}
-						}else{
-							std::cout << "error : forname" << std::endl;
-							return nullptr;
+						//名取得。
+						STLString t_name = this->GetNameFromContentsItem(t_content_item);
+						if(t_name.length() <= 0){
+							return false;
 						}
 
-						if(t_content_item->IsExistItem("formvalue") == true){
-							if(t_content_item->GetItem("formvalue")->GetValueType() == JsonItem::ValueType::StringData){
-								t_formvalue = *t_content_item->GetItem("formvalue")->GetStringData();
-								if(t_formvalue.length() > 0){
-									std::cout << "formvalue : " << t_formvalue << std::endl;
-								}else{
-									std::cout << "error : formvalue" << std::endl;
-									return nullptr;
-								}
-							}else{
-								std::cout << "error : formvalue" << std::endl;
-								return nullptr;
-							}
-						}else{
-							std::cout << "error : formvalue" << std::endl;
-							return nullptr;
+						//値取得。
+						STLString t_value = this->GetValueFromContentsItem(t_content_item);
+						if(t_value.length() <= 0){
+							return false;
 						}
 
 						//コンテンツの追加。
-						this->http->AddPostContent(t_formname,t_formvalue);
+						this->http->AddPostContent(t_name,t_value);
 
 					}else{
 						std::cout << "error : type" << std::endl;
@@ -397,28 +478,14 @@ namespace NApp
 		{
 			sharedptr<NBlib::FileHandle> t_out_file(new NBlib::FileHandle());
 			{
-				STLWString t_output_filename;
-
-				if(this->config->IsExistItem("output") == true){
-					if(this->config->GetItem("output")->GetValueType() == JsonItem::ValueType::StringData){
-						CharToWchar(*this->config->GetItem("output")->GetStringData(),t_output_filename);
-						if(t_output_filename.length() > 0){
-							std::cout << "output : " << WcharToChar(t_output_filename) << std::endl;
-						}else{
-							std::cout << "error : output" << std::endl;
-							return false;
-						}
-					}else{
-						std::cout << "error : output" << std::endl;
-						return false;
-					}
-				}else{
-					std::cout << "error : output" << std::endl;
+				STLWString t_output_filename = this->GetOutPutFromConfig();
+				if(t_output_filename.length() <= 0){
 					return false;
 				}
 				
-				std::cout << "open : " << WcharToChar(t_output_filename) << std::endl;
-				if(t_out_file->WriteOpen(t_output_filename) == false){
+				if(t_out_file->WriteOpen(t_output_filename) == true){
+					std::cout << "open : " << WcharToChar(t_output_filename) << std::endl;
+				}else{
 					std::cout << "error : " << WcharToChar(t_output_filename) << std::endl;
 					return false;
 				}
@@ -444,21 +511,24 @@ namespace NApp
 						}
 					}
 				}else{
-					if(this->http->IsError() == true){
-						std::cout << "connect : error : " << std::endl;
-					}
-
 					std::cout << "connect : statuscode : " << this->http->GetStatusCode() << std::endl;
 
-					std::cout << "connect : end" << std::endl;
+					bool t_is_error = this->http->IsError();
 					this->http->ConnectEnd();
-
 					this->http.reset();
-					break;
+
+					if(t_is_error == true){
+						std::cout << "connect : error : " << std::endl;
+						return false;
+					}else{
+						std::cout << "connect : success" << std::endl;
+						return true;
+					}
 				}
 			}
 
-			return true;
+			ASSERT(0);
+			return false;
 		}
 	};
 
@@ -488,6 +558,8 @@ namespace NApp
 					}
 				}
 			}
+
+			std::cout << "end" << std::endl;
 		}
 
 		//通信。終了。
