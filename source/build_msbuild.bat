3585457c254a72dc4e1bd59659d1c1d72cf1a860
blueback <blueback28@gmail.com>
2017-12-31 22:39:16 +0900

    整理

--- source/build_msbuild.bat
+++ source/build_msbuild.bat
@@ -5,9 +5,9 @@ CALL ..\..\..\setting.bat
 FOR /F %%C IN (configuration.txt) DO (
 	FOR /F %%P IN (platform.txt) DO (
 		@REM ビルド。
-		%MSBUILD% postconnect.vcxproj /t:rebuild /p:Configuration=%%C;Platform=%%P
+		%MSBUILD% postconnect.vcxproj /t:rebuild /p:Configuration=%%C;Platform=%%P >> log.txt
 
 		@REM 移動。
-		move .\bin\%%P\%%C\postconnect.exe postconnect_%%P_%%C.exe
+		move .\bin\%%P\%%C\postconnect.exe postconnect_%%P_%%C.exe >> log.txt
 	)
 )
